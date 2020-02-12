7c355536a90073052051109555c56b23a90a14cf
Rafael_Krb <krbashianrafael@gmail.com>
2020-02-12 21:46:41 +0200

    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ fix

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
 
-import burgerBuilderReducer from "./store/reducers/burgerBuilder";
-import orderReducer from "./store/reducers/order";
-import authReducer from "./store/reducers/auth";
+import burgerBuilderReducer from './store/reducers/burgerBuilder'
+import orderReducer from './store/reducers/order'
+import authReducer from './store/reducers/auth'
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
-  auth: authReducer
-});
+  auth: authReducer,
+})
 
 const composeEnhancers =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : compose
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
   <Provider store={store}>
@@ -36,8 +33,8 @@ const app = (
       <App />
     </BrowserRouter>
   </Provider>
-);
+)
 
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(app, document.getElementById('root'))
 
-serviceWorker.unregister();
+serviceWorker.unregister()
